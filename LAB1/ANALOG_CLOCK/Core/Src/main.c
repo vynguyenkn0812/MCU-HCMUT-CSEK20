b62fc05fb82ecc3ad4b2fb1c135380595962af81
Nguyễn Kim Ngọc Vy <86225482+vynguyenkn0812@users.noreply.github.com>
2022-09-21 23:48:05 +0700

    Ex8

--- LAB1/ANALOG_CLOCK/Core/Src/main.c
+++ LAB1/ANALOG_CLOCK/Core/Src/main.c
@@ -71,21 +71,22 @@ void clearAllClock() {
 
 void setNumberOnClock(int num) {
 	switch(num) {
-		case 0: HAL_GPIO_WritePin(NUM_0_GPIO_Port, NUM_0_Pin, RESET); break;
-		case 1: HAL_GPIO_WritePin(NUM_1_GPIO_Port, NUM_1_Pin, RESET); break;
-		case 2: HAL_GPIO_WritePin(NUM_2_GPIO_Port, NUM_2_Pin, RESET); break;
-		case 3: HAL_GPIO_WritePin(NUM_3_GPIO_Port, NUM_3_Pin, RESET); break;
-		case 4: HAL_GPIO_WritePin(NUM_4_GPIO_Port, NUM_4_Pin, RESET); break;
-		case 5: HAL_GPIO_WritePin(NUM_5_GPIO_Port, NUM_5_Pin, RESET); break;
-		case 6: HAL_GPIO_WritePin(NUM_6_GPIO_Port, NUM_6_Pin, RESET); break;
-		case 7: HAL_GPIO_WritePin(NUM_7_GPIO_Port, NUM_7_Pin, RESET); break;
-		case 8: HAL_GPIO_WritePin(NUM_8_GPIO_Port, NUM_8_Pin, RESET); break;
-		case 9: HAL_GPIO_WritePin(NUM_9_GPIO_Port, NUM_9_Pin, RESET); break;
-		case 10: HAL_GPIO_WritePin(NUM_10_GPIO_Port, NUM_10_Pin, RESET); break;
-		case 11: HAL_GPIO_WritePin(NUM_11_GPIO_Port, NUM_11_Pin, RESET); break;
+		case 0: HAL_GPIO_WritePin(NUM_0_GPIO_Port, NUM_0_Pin, SET); break;
+		case 1: HAL_GPIO_WritePin(NUM_1_GPIO_Port, NUM_1_Pin, SET); break;
+		case 2: HAL_GPIO_WritePin(NUM_2_GPIO_Port, NUM_2_Pin, SET); break;
+		case 3: HAL_GPIO_WritePin(NUM_3_GPIO_Port, NUM_3_Pin, SET); break;
+		case 4: HAL_GPIO_WritePin(NUM_4_GPIO_Port, NUM_4_Pin, SET); break;
+		case 5: HAL_GPIO_WritePin(NUM_5_GPIO_Port, NUM_5_Pin, SET); break;
+		case 6: HAL_GPIO_WritePin(NUM_6_GPIO_Port, NUM_6_Pin, SET); break;
+		case 7: HAL_GPIO_WritePin(NUM_7_GPIO_Port, NUM_7_Pin, SET); break;
+		case 8: HAL_GPIO_WritePin(NUM_8_GPIO_Port, NUM_8_Pin, SET); break;
+		case 9: HAL_GPIO_WritePin(NUM_9_GPIO_Port, NUM_9_Pin, SET); break;
+		case 10: HAL_GPIO_WritePin(NUM_10_GPIO_Port, NUM_10_Pin, SET); break;
+		case 11: HAL_GPIO_WritePin(NUM_11_GPIO_Port, NUM_11_Pin, SET); break;
 		default:
 	}
 }
+
 /* USER CODE END 0 */
 
 /**
