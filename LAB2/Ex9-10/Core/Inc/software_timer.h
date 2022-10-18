6a0fe8a949630b5d5f597a75609a06c8960a8ba9
Nguyễn Kim Ngọc Vy <86225482+vynguyenkn0812@users.noreply.github.com>
2022-10-18 18:09:56 +0700

    Ex9-10

---
+++ LAB2/Ex9-10/Core/Inc/software_timer.h
@@ -0,0 +1,20 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 18, 2022
+ *      Author: PC
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer0_flag;
+extern int timer1_flag;
+
+void setTimer0(int duration);
+void setTimer1(int duration);
+
+void timer_run();
+
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
