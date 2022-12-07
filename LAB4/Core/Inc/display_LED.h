69873f0ea60701236fc49c2f697d123c072c927f
Nguyễn Kim Ngọc Vy <86225482+vynguyenkn0812@users.noreply.github.com>
2022-12-07 20:33:03 +0700

    Upload display_LED file

---
+++ LAB4/Core/Inc/display_LED.h
@@ -0,0 +1,20 @@
+/*
+ * display_LED.h
+ *
+ *  Created on: Dec 4, 2022
+ *      Author: PC
+ */
+
+#ifndef INC_DISPLAY_LED_H_
+#define INC_DISPLAY_LED_H_
+
+#include "main.h"
+#include "global.h"
+
+void ToggleRed();
+void ToggleYellow();
+void ToggleGreen();
+void ToggleOne_shot();
+void ToggleWhenButtonPressed();
+
+#endif /* INC_DISPLAY_LED_H_ */
---
+++ LAB4/Core/Src/display_LED.c
@@ -0,0 +1,28 @@
+/*
+ * display_LED.c
+ *
+ *  Created on: Dec 4, 2022
+ *      Author: PC
+ */
+
+#include "display_LED.h"
+
+void ToggleRed() {
+	HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+}
+
+void ToggleYellow() {
+	HAL_GPIO_TogglePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin);
+}
+
+void ToggleGreen() {
+	HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+}
+
+void ToggleOne_shot() {
+	HAL_GPIO_TogglePin(ONE_SHOT_LED_GPIO_Port, ONE_SHOT_LED_Pin);
+}
+
+void ToggleWhenButtonPressed() {
+	HAL_GPIO_TogglePin(LED_Pressed_GPIO_Port, LED_Pressed_Pin);
+}
