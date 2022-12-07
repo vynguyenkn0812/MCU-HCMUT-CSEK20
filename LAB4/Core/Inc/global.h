3ac248be608716f348acdd2d61a5758a70225b85
Nguyễn Kim Ngọc Vy <86225482+vynguyenkn0812@users.noreply.github.com>
2022-12-07 22:29:55 +0700

    Upload global file

---
+++ LAB4/Core/Inc/global.h
@@ -0,0 +1,13 @@
+/*
+ * global.h
+ *
+ *  Created on: Dec 4, 2022
+ *      Author: PC
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#define TIME_CYCLE 10 //*TICKS*
+
+#endif /* INC_GLOBAL_H_ */
