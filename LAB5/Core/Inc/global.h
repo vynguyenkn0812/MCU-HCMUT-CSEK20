4baa7eec97f1e9f0258b76aca7b5d4d20617b297
Nguyễn Kim Ngọc Vy <86225482+vynguyenkn0812@users.noreply.github.com>
2022-12-27 23:20:54 +0700

    update global

--- LAB5/Core/Inc/global.h
+++ LAB5/Core/Inc/global.h
@@ -8,6 +8,32 @@
 #ifndef INC_GLOBAL_H_
 #define INC_GLOBAL_H_
 
+#define MAX_BUFFER_SIZE 30 //MAX UART BUFFER
+
+#define INIT 				0
+
+// status of command parser
+#define WAIT_TOKEN 			1
+#define RECEIVE_DATA		2
+
+// status of UART
+#define WAIT_COMMAND_RST 	11
+#define SEND_DATA			12
+
+extern uint8_t buffer_rx[MAX_BUFFER_SIZE];
+extern uint8_t buffer_tx[MAX_BUFFER_SIZE];
+extern uint8_t command_parser_data[MAX_BUFFER_SIZE];
+
+extern int status_parser;
+extern int status_communicate;
+
+extern uint8_t index_buffer;
+extern uint8_t buffer_flag;
+
+extern uint8_t temp;
+extern uint8_t ADC_value;
+
+extern int cmd_flag;
 
 
 #endif /* INC_GLOBAL_H_ */
--- LAB5/Core/Src/global.c
+++ LAB5/Core/Src/global.c
@@ -5,4 +5,19 @@
  *      Author: PC
  */
 
+#include "global.h"
 
+uint8_t buffer_rx[MAX_BUFFER_SIZE] = {0};
+uint8_t buffer_tx[MAX_BUFFER_SIZE] = {0};
+uint8_t command_parser_data[MAX_BUFFER_SIZE] = {0};
+
+int status_parser = 0;
+int status_communicate = 0;
+
+uint8_t index_buffer = 0;
+uint8_t buffer_flag = 0;
+
+uint8_t temp = 0;
+uint8_t ADC_value = 0;
+
+int cmd_flag = 0;
